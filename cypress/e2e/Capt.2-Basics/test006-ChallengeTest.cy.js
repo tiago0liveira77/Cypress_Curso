5997137a2128c82018450b70d4eeff7dca2b27e2
xn0tBad <128006340+xn0tBad@users.noreply.github.com>
2023-04-11 16:46:30 +0100

    Maneira correta usando context block

--- cypress/e2e/Capt.2-Basics/test006-ChallengeTest.cy.js
+++ cypress/e2e/Capt.2-Basics/test006-ChallengeTest.cy.js
@@ -1,23 +1,27 @@
-describe('Browser Actions', () => {
-    it('should load books website', () => {
-        cy.visit('https://books.toscrape.com/index.html', {timeout:10000});
+
+
+describe('Testes a books.toscrape', () => {
+    beforeEach(() => {
+        cy.visit('https://books.toscrape.com/index.html');
+    })
+    it('should check URL', () => {
         cy.url().should('include', 'index.html');
     });
+    context('Verificar Preço do livro "Olio"', () => {
+        
 
-    it('should click poetry category', () => {
-        cy.visit('https://books.toscrape.com/index.html', {timeout:10000});
-        cy.url().should('include', 'index.html');
-        cy.get('a').contains('Poetry').click();
-        cy.get('h1').contains('Poetry');
-    });
+        it('should click poetry category', () => {
+            cy.get('a').contains('Poetry').click();
+            cy.get('h1').contains('Poetry');
+        });
 
-    it('should click on olio book', () => {
-        cy.get('a').contains('Olio').click();
-    });
+        it('should click on olio book', () => {
+            cy.get('a').contains('Olio').click();
+        });
 
-    it('should have correct price 23.88', () => {
-        cy.get('.price_color').contains('23.88');
-    });
-
-  })
+        it('should have correct price 23.88', () => {
+            cy.get('.price_color').contains('23.88');
+        });
+    })
+})
   
